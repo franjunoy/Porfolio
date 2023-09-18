f36e9da406bb76cf3866759dfbdfb80e41529bf1
franjunoy <franjunoy@hotmail.com>
2023-09-18 12:46:56 -0300

    Configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/app.",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fran: "#ce0002",
+      },
+    },
   },
   plugins: [],
-}
-
+};
